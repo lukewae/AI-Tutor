e5d2eb058c768ea5fd1b9e8346b90eb1ae8f3431
lukewae <luke@ives.net.au>
2025-03-20 06:31:22 +1000

    Database

--- src/main/java/com/example/bugs/HelloController.java
+++ src/main/java/com/example/bugs/HelloController.java
@@ -1,44 +1,14 @@
 package com.example.bugs;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-
-import java.io.IOException;
+import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private TextArea termsAndConditions;
-    @FXML
-    private Button nextButton;
+    private Label welcomeText;
 
     @FXML
-    public void initialize() {
-        termsAndConditions.setText("Your terms and conditions text here...");
-    }
-
-    // Fix this method signature - must take ActionEvent or no parameters
-    @FXML
-    private void onAgreeCheckBoxClick() {  // Changed from (CheckBox agreeCheckBox)
-        boolean accepted = ((CheckBox) nextButton.getScene().lookup("#agreeCheckBox")).isSelected();
-        nextButton.setDisable(!accepted);
-    }
-
-    @FXML
-    private void onNextButtonClick() throws IOException {
-        Stage stage = (Stage) nextButton.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
-        stage.setScene(scene);
-    }
-
-    @FXML
-    private void onCancelButtonClick() {
-        Stage stage = (Stage) nextButton.getScene().getWindow();
-        stage.close();
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
