a31bef77ce1197dcfe780c6863d824d08c6e36d1
mitch <mitch005thomas@gmail.com>
2025-04-03 17:14:40 +1000

    Adds basic database features

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
-module com.example.bugs {
+module com.example.addressbook {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.bugs to javafx.fxml;
     exports com.example.bugs;
+    exports com.example.bugs.controller;
+    opens com.example.bugs.controller to javafx.fxml;
+    exports com.example.bugs.model;
+    opens com.example.bugs.model to javafx.fxml;
 }
